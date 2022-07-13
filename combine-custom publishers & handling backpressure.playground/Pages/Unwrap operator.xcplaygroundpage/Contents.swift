c313551db0e25d076b6d1424d8d1150f7fcac26b
Yuhsuan, Chen <49970020+nyto9999@users.noreply.github.com>
2022-07-14 01:20:57 +0800

    Custom Publishers & Handling Backpressure

---
+++ combine-custom publishers & handling backpressure.playground/Pages/Unwrap operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,17 @@
+import Combine
+import Foundation
+
+extension Publisher {
+    
+    func unwrap<T>() -> Publishers.CompactMap<Self, T> where Output == Optional<T> {
+        compactMap { $0 }
+    }
+}
+
+let values: [Int?] = [1,2,nil,3,nil,4]
+
+values.publisher
+    .unwrap()
+    .sink {
+        print("Received value: \($0)")
+    }
